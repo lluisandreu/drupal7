4a16cc19971e0d6d29c051e602ebc2a11db594e6
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2016-01-28 15:55:55 +0100

    First commit

---
+++ sites/tavernacalbarber.cat/themes/calbarber_theme/template.php
@@ -0,0 +1,37 @@
+<?php
+
+// Gets background image from node
+function calbarber_theme_preprocess_page(&$variables) {
+	if (arg(0) == "node" && is_numeric(arg(1))) {
+		$nodeid = arg(1);
+		$thenode = node_load($nodeid);
+		//dpm($thenode);
+		if(!empty($thenode->field_page_bgimage)) {	
+			$image_style_path = image_style_url("background_image", $thenode->field_page_bgimage['und'][0]['uri']);
+			drupal_add_css("@media (min-width: 650px) {.main-section {background-image: url('$image_style_path') !important}}", array(
+				'type' => 'inline',
+				'group' => 'CSS_THEME',
+				'preprocess' => FALSE,
+				'media' => 'screen',
+				)
+			);
+		}
+		if(!empty($thenode->field_page_hide_content) && $thenode->field_page_hide_content['und'][0]['value'] == 1) {
+			drupal_add_css(".not-logged-in .main-content-box { 
+				position: absolute; 
+				height: 1px;
+				width: 1px;
+				overflow: hidden;
+			}", array(
+				'type' => 'inline',
+				'group' => 'CSS_THEME',
+				'preprocess' => FALSE,
+				'media' => 'screen',
+				));
+		}
+	}
+}
+
+function calbarber_theme_preprocess_html(&$variables) {
+	drupal_add_css("//fonts.googleapis.com/css?family=Just+Me+Again+Down+Here", array('group' => CSS_THEME));
+}
---
+++ sites/tavernacalbarber.cat/themes/calbarber_theme/templates/nodes/node--page.tpl.php
@@ -0,0 +1,28 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="node-content">
+<?php if(!empty($node->field_page_hide_content) && $node->field_page_hide_content['und'][0]['value'] == 1): ?>
+  <div class="messages info">El contingut d'aquesta pàgina és invisible als usuaris anònims</div>
+<?php endif; ?>
+<?php if(isset($content['body'])): ?>
+  <div class="node-content-left"<?php print $content_attributes; ?>>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h1><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php
+      print render($content['body']);
+    ?>
+  </div>
+<?php endif; ?>
+  <?php if(isset($content['field_page_images'])): ?>
+    <div class="node-content-right <?php if(isset($content['body'])) {print "body";} ?>">
+      <?php if(!isset($content['body'])): ?>
+          <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($content['field_page_images']); ?>
+    </div>
+  <?php endif; ?>
+  </div>
+</div>
+</div>
