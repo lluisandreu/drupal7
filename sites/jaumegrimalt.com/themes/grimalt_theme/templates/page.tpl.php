bf7a60b3e4aed4c9de6d188edc87e247392f4f4d
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2016-03-14 12:12:04 +0100

    Jaume Grimalt Datastore

--- sites/jaumegrimalt.com/themes/grimalt_theme/templates/page.tpl.php
+++ sites/jaumegrimalt.com/themes/grimalt_theme/templates/page.tpl.php
@@ -1,15 +1,38 @@
 <div class="off-canvas-wrap" data-offcanvas>
+<?php if ($top_bar): ?>
+  <!--.top-bar -->
+  <?php if ($top_bar_classes): ?>
+    <div class="<?php print $top_bar_classes; ?>">
+  <?php endif; ?>
+  <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
+    <ul class="title-area">
+      <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
+      <li class="toggle-topbar menu-icon">
+        <a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
+    </ul>
+    <section class="top-bar-section">
+      <?php if ($top_bar_main_menu) : ?>
+        <?php print $top_bar_main_menu; ?>
+      <?php endif; ?>
+    </section>
+  </nav>
+  <?php if ($top_bar_classes): ?>
+    </div>
+  <?php endif; ?>
+  <!--/.top-bar -->
+<?php endif; ?>
+
   <div class="inner-wrap">
     <div class="main-content">
       <header class="section-header">
         <div class="gradient">
         <div class="row">
-          <div class="logo large-4 columns">
+          <div class="logo medium-4 small-12 columns">
             <a href="<?php print $front_page; ?>">
               <img src="/<?php print drupal_get_path("theme", "grimalt_theme") . '/logo.svg'?>" alt="<?php print $site_name; ?>">
             </a>
           </div>
-          <div class="header-right large-8 columns">
+          <div class="header-right medium-8 columns show-for-medium-up">
             <?php if($page['header']): ?>
             <div class="header-right-content">
               <?php print render($page['header']); ?>
