a8a483d1c9fccd3a3a48a2c17ccd8500f57be99b
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2016-04-29 09:43:26 +0200

    Some module updates

--- sites/jaumegrimalt.com/themes/grimalt_theme/template.php
+++ sites/jaumegrimalt.com/themes/grimalt_theme/template.php
@@ -21,8 +21,8 @@ function STARTER_preprocess_node(&$variables) {
 function grimalt_theme_form_alter(&$form, &$form_state, $form_id) {
 	switch ($form_id) {
 		case 'views_exposed_form':
-			$form['search_api_views_fulltext']['#prefix'] = "<div class='small-8 medium-10 columns'>";
-			$form['search_api_views_fulltext']['#attributes']['placeholder'] = t('Busca un producto o servicio...');
+			$form['search']['#prefix'] = "<div class='small-8 medium-10 columns'>";
+			$form['search']['#attributes']['placeholder'] = t('Busca un producto o servicio...');
 			$form['search_api_views_fulltext']['#suffix'] = "</div>";
 			$form['submit']['#prefix'] = "<div class='small-4 medium-2 columns'>";
 			$form['submit']['#suffix'] = "</div>";
