c4d7be749dd6cf6750811faf4212cf05fddb9b8d
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2016-06-10 16:55:59 +0200

    New site, David Querol

---
+++ sites/davidquerol.com/themes/davidquerol_theme/templates/nodes/node--project.tpl.php
@@ -0,0 +1,35 @@
+<?php 
+dpm($node); 
+?>
+
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<h1><?php print $node->title; ?></h1>
+
+<?php if(isset($content['body'])): ?>
+  <?php print render($content['body']); ?>
+<?php endif; ?>
+<div class="project-media">
+  <?php if(isset($node->field_project_display)): ?>
+    <?php $did = $node->field_project_display['und'][0]['value']; ?>
+    <?php if($did == 0): ?> 
+     <div class="project-media-vertical">
+       <?php print render($content['field_images']); ?>
+     </div>
+    <?php elseif($did == 1): ?>
+      <div class="project-media-horizontal">
+        <?php print render($content['field_images']); ?>
+      </div>
+    <?php elseif($did == 2): ?> 
+      <div class="project-media-colorbox clearfix">
+        <?php print render($content['field_images']); ?>
+      </div>
+    <?php elseif($did == 3): ?>
+      <div class="project-media-slideshow">
+        <?php print render($content['field_images']); ?>
+      </div>
+    <?php endif; ?>
+  <?php endif; ?>
+
+</div>
+
+</div>
