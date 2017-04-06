059b07834db9fa5b6f1366a95f21b2c049c35919
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-04-06 14:28:19 +0200

    Changes to THS

--- sites/ths-thehorseshop.com/themes/ths/js/ths.js
+++ sites/ths-thehorseshop.com/themes/ths/js/ths.js
@@ -5,6 +5,16 @@
     $(document).ready(function () {
 
         toggleMenu();
+
+        $('.site-search a').on('click', function (event) {
+            event.preventDefault();
+            if ($(this).find('i').hasClass('fa-search')) {
+                $(this).find('i').removeClass('fa-search').addClass('fa-times');
+            } else {
+                $(this).find('i').removeClass('fa-times').addClass('fa-search');
+            }
+            $('.search-block').toggleClass('expanded');
+        });
     });
 
     function toggleMenu() {
